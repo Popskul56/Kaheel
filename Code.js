75c8d545f0281fc94871b89ea2a03b5f0bb8a405
Popskul56 <112734438+Popskul56@users.noreply.github.com>
2022-10-24 10:54:58 -0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,66 @@
+function runtimer (x){currTime=50;
+    timeout=5000;
+    
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000;
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000; 
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000; 
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000; 
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000;
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000; 
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000; 
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000; 
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000;  
+    setTimeout(function(){
+        x.innerHTML=currTime;
+        currTime=currTime-5;
+    },timeout);
+    timeout=timeout+5000;
+    setTimeout(function(){
+       alert("Blastoff");
+    },timeout);
+    timeout=timeout+5000;
+
+
+}
+function startbuttonclick(){}
+function stopbuttonclick(){}
+
+
+
+
